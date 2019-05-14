3b9caa66eb1d70e264d01b266320a3aee78a82c5
Nick Darlow <dangerous.darlow@gmail.com>
2019-05-14 23:27:03 +0100

    Add app bar to the top of the page [#165949352]

---
+++ src/components/TopBar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconButton } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+import logo from './../logo.svg';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function TopBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit">
+            <img src={logo} alt="logo" width="48" height="48"></img>
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            hi there
+          </Typography>
+          <IconButton>
+            <FontAwesomeIcon icon={faSignInAlt}></FontAwesomeIcon>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+TopBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TopBar);
